a408ea6080c14d47ccc6fc2847e7942d9144efd4
Xolani <mazi76erx@gmail.com>
2025-02-26 11:08:29 +0000

    Updated navbar with additional dropdowns, dark mode gradients, and notifications

--- app/types.ts
+++ app/types.ts
@@ -1,8 +1,14 @@
 import React from "react";
 
+export interface MenuItemSimple {
+  label: string;
+  href: string;
+}
+
 export interface MenuItem {
   label: string;
   href: string;
+  submenu?: MenuItemSimple[];
 }
 
 export interface Feature {
